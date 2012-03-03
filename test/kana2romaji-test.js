c714b701bb09ce5592ed9e0dcf4c65419751a816
kawasima <kawasima1016@gmail.com>
2012-03-04 08:27:22 +0900

    Fix the character mapping according to http://www.seikatubunka.metro.tokyo.jp/hebon/

--- test/kana2romaji-test.js
+++ test/kana2romaji-test.js
@@ -8,3 +8,6 @@ console.log('Testing kana2romaji');
 assert.equal(kana2romaji.kana2romaji("ハットリ ハンゾウ"), "hattori hanzo");
 assert.equal(kana2romaji.kana2romaji("シンバシ"), "shimbashi");
 assert.equal(kana2romaji.kana2romaji("オオコウチ"), "okochi");
+assert.equal(kana2romaji.kana2romaji("ジェームスディーン"), "jiemusudein");
+assert.equal(kana2romaji.kana2romaji("マンギョンボンゴウ"), "mangyombongo");
+assert.equal(kana2romaji.kana2romaji("ミャニュフェスチョ"), "myanyufuesucho");
